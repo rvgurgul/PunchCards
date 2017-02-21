6cae8643c716896f80e2a95d66124e301f068e34
Student <student@rgurgul.local>
2017-02-21 09:28:16 -0600

    Initial Commit

---
+++ PunchCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PunchCards
+//
+//  Created by Richie Gurgul on 2/21/17.
+//  Copyright © 2017 Richie Gurgul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
